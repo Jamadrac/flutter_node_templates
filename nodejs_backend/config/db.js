567fb396354bc12ffcd44c39dbe1f19bdc1d8736
jamadrac <rabbikindalo001@gmail.com>
2024-03-20 06:06:51 +0200

    sd

--- nodejs_backend/config/db.js
+++ nodejs_backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.set('debug', true);
 
 
-const connection = mongoose.createConnection(`mongodb://127.0.0.1:27017/ToDoDB`).on('open',()=>{console.log("MongoDB Connected");}).on('error',()=>{
+const connection = mongoose.createConnection(`mongodb+srv://rabbi:jamadrac@cluster0.rdngft1.mongodb.net/?retryWrites=true&w=majority`).on('open',()=>{console.log("MongoDB Connected");}).on('error',()=>{
     console.log("MongoDB Connection error");
 });
 
